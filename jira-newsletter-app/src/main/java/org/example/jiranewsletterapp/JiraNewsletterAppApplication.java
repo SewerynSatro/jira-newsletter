48d870e26d1beb564829e872c4457ff0f72463d7
Seweryn Satro <sewerynsatro@gmail.com>
2025-05-20 18:43:12 -0400

    Created project structure using Maven, Spring Boot, JPA, PostgreSQL, Lombok

---
+++ jira-newsletter-app/src/main/java/org/example/jiranewsletterapp/JiraNewsletterAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.jiranewsletterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraNewsletterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraNewsletterAppApplication.class, args);
+    }
+
+}
---
+++ jira-newsletter-app/src/test/java/org/example/jiranewsletterapp/JiraNewsletterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jiranewsletterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraNewsletterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
